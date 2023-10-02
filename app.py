77b2ce6098c8104b169c758d77393274c0c458d3
Anish kumar <76154926+19cs1h4261@users.noreply.github.com>
2023-10-02 22:28:35 +0530

    Update app.py
    Enabled insert methods

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def get_db():
 @app.post('/getContentsList/')
 def get_contents(selected_language : dict):
   selected_language = selected_language.get('language')
-  # insert_contents_list(CONTENTS_LIST)
+  insert_contents_list(CONTENTS_LIST)
   data = session.query(ContentsList).filter(ContentsList.language== selected_language.lower()).all()
   return data
 
@@ -39,7 +39,7 @@ def get_contents(selected_language : dict):
 @app.post('/getContents/')
 def get_contents(selected_content : dict):
   selected_content = selected_content.get('language_content')
-  # insert_contents(CONTENTS_PAGE)
+  insert_contents(CONTENTS_PAGE)
   data = session.query(Contents).filter(Contents.language_content== selected_content).all()
   return data
 
